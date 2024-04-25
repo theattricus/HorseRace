cbbcc35af3338cca94f9803fb70ea7578f419e01
erik <atherixrs@gmail.com>
2024-04-26 00:39:33 +0100

    Test 4 lanes, 1230

--- Part1/Part1.java
+++ Part1/Part1.java
@@ -1,10 +1,10 @@
 public class Part1 {
 	public static void main(String[] args) {
-		Race race = new Race(20);
-		race.setLanes(6);
-		Horse horse1 = new Horse('♘', "PIPPI LONGSTOCKING", 0.6);
-		Horse horse2 = new Horse('♞', "KOKOMO", 0.6);
-		Horse horse3 = new Horse('♘', "EL JEFE", 0.6);
+		Race race = new Race(50);
+		race.setLanes(4);
+		Horse horse1 = new Horse('♘', "PIPPI LONGSTOCKING", 0.3);
+		Horse horse2 = new Horse('♞', "KOKOMO", 0.3);
+		Horse horse3 = new Horse('♘', "EL JEFE", 0.3);
 		race.addHorse(horse1, 1);
 		race.addHorse(horse2, 2);
 		race.addHorse(horse3, 3);
